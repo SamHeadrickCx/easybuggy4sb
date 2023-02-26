cc8e2315271f02c9f059c5f2aee889d79757cfa1
SamHeadrickCx <96081318+SamHeadrickCx@users.noreply.github.com>
2023-02-26 14:44:23 -0500

    Update Config.java

--- src/main/java/org/t246osslab/easybuggy4sb/Config.java
+++ src/main/java/org/t246osslab/easybuggy4sb/Config.java
@@ -1,5 +1,5 @@
 package org.t246osslab.easybuggy4sb;
-
+// Config Constants
 public class Config {
     public static final String APP_ROOT = "/eb/v1";
     public static final String TEMPLATE_PREFIX = "eb/v1/";
